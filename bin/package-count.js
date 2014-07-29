timoxley/pkgcount

commit fd3345cfb24272a893112fb7fd97dd9997de2b47

Change --min to be '>=' instead of '>'.

bin/package-count.js
@@ -13,13 +13,13 @@ var pkgcount = require('../')
 
 program
   .version(require('../package.json').version)
-  .option('--min [count]', 'Only list packages appearing more than this number of times')
+  .option('--min [count]', 'Only list packages duplicated at least this number of times')
   .option('--duplicates', 'Only list packages appearing more than once')
   .option('--json', 'Display json output')
-  .option('--no-color', 'Displays uncolored output.')
-  .option('--paths', 'Display paths to each package, instead of total count')
+  .option('--no-color', 'Display uncolored output.')
+  .option('--paths', 'List paths to each package instead of counts')
   .option('--summary', 'Only display summary')
-  .option('--depth [depth]', 'Depth to analyse')
+  .option('--depth [depth]', 'Limit traversal depth')
   .parse(process.argv);
 
 var names = program.args

lib/filter-packages-count.js
@@ -5,7 +5,7 @@ module.exports = function nameFilter(paths, min) {
   var result = {}
   for (var key in paths) {
     var count = paths[key].length
-    if (count <= min) continue
+    if (count < min) continue
     result[key] = paths[key]
   }
   return result

commit cd15ca09443f11995705ad648315f58f4e82761b

Fix --duplicates behaviour.
Bug introduced in fd3345c: "Change --min to be '>=' instead of '>'"

Closes #3.

bin/package-count.js
@@ -24,7 +24,7 @@ program
 
 var names = program.args
 var dir = process.cwd()
-if (program.duplicates) program.min = program.min || 1
+if (program.duplicates) program.min = program.min || 2
 
 pkgcount(dir, {depth: program.depth}, function(err, allPaths) {
   console.log("")